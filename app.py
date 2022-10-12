e8f9e2f74496aab189fb828c18c1e15b21c570d6
jkap86 <jkap86@gmail.com>
2022-10-11 20:28:44 -0400

    -

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from asyncio import futures
 from urllib import request
 from flask import Flask
 import requests
@@ -6,26 +7,27 @@ import concurrent.futures
 app = Flask(__name__)
 
 
+def getLeagueInfo(league):
+    users = requests.get(
+        'https://api.sleeper.app/v1/league/' + str(league['league_id']) + '/users', timeout=3).json()
+
+    rosters = requests.get(
+        'https://api.sleeper.app/v1/league/' + str(league['league_id']) + '/rosters').json()
+
+    league_detail = {
+        'users': users,
+        'rosters': rosters
+    }
+
+    return league_detail
+
+
 @app.route('/')
 def index():
     leagues = requests.get(
         'https://api.sleeper.app/v1/user/424024949334740992/leagues/nfl/2022', timeout=3).json()
 
-    def getLeagueInfo(league):
-        users = requests.get(
-            'https://api.sleeper.app/v1/league/' + str(league['league_id']) + '/users', timeout=3).json()
+    with concurrent.futures.ProcessPoolExecutor(max_workers=10) as executor:
+        results = executor.map(getLeagueInfo, leagues)
 
-        rosters = requests.get(
-            'https://api.sleeper.app/v1/league/' + str(league['league_id']) + '/rosters').json()
-
-        league_detail = {
-            'users': users,
-            'rosters': rosters
-        }
-
-        return league_detail
-
-    with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
-        leagues_detailed = list(executor.map(getLeagueInfo, leagues))
-
-    return leagues_detailed
+    return list(results)
